aed7639725b4069b65bef7615bdd35145fab59e0
vtrejo25 <vtrejo@zagmail.gonzaga.edu>
2025-02-11 18:05:46 -0800

    update

---
+++ main.py
@@ -0,0 +1,8 @@
+def greet():
+    print("Hello from main.py!")
+
+if __name__ == '__main__':
+    print("This script is being run directly.")
+    def greet_2():
+        print("greet 2!")
+    greet()
